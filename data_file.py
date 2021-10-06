2c408b75beb59c5c25d263b62440d4b9a0259c80
pantham.naveen <naveen@mosaikanalytics.com>
2021-10-06 12:14:08 +0530

    input data added in separate file i.e. data_file.py

---
+++ data_file.py
@@ -0,0 +1,12 @@
+bmi_list =[
+    {"Gender": "Male", "HeightCm": 175, "WeightKg": 75 }, 
+    {"Gender": "Male", "HeightCm": 171, "WeightKg": 96 }, 
+    { "Gender": "Male", "HeightCm": 161, "WeightKg":85 }, 
+    { "Gender": "Male", "HeightCm": 180, "WeightKg": 77 }, 
+    { "Gender": "Female", "HeightCm": 166,"WeightKg": 62}, 
+    {"Gender": "Female", "HeightCm": 150, "WeightKg": 70}, 
+    {"Gender": "Female","HeightCm": 167, "WeightKg": 82}
+]
+
+health_risk = ['Malnutrition risk', 'Low risk', 'Enhanced risk', 'Medium risk', 'High risk', 'Very high risk']
+bmi_category = ['Underweight', 'Normal weight', 'Overweight', 'Moderately obese', 'Severely obese', 'Very severely obese']
